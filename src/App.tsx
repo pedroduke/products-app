62134d3c251a36b869a428a0a4a55f5de572ed9d
Pedro Duque <pedroduque8@gmail.com>
2023-03-07 11:39:20 +0100

    init route config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
 
+import Header from './components/Header';
+import HomePage from './pages/HomePage';
+import ProductDetailPage from './pages/ProductDetailPage';
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/product/:id' element={<ProductDetailPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <h1 style={{ borderBottom: '1px solid green' }}>Header</h1>;
+};
+
+export default Header;
